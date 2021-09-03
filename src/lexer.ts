161a3fe870654af0842b470b19b068de03ecf522
Brian Angulo <brianangulo96@gmail.com>
2021-09-03 16:51:05 -0500

    Finished working lexer

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,14 +1,46 @@
+import { readFileSync } from "fs";
+
 interface Token {
-   type: string;
-   regex: RegExp; 
+  tag: string;
+  content: string;
 }
 
-const tokensList: Token[] = [
-    {
-        type: "htmlTag",
-        regex: /(?<=::).*?(?=::)/g
-    }, {
-        type: "content",
-        regex: /a/,
-    }
-]
+//keeping regex file global
+const regexTag: RegExp = /(?<=::).*?(?=::)/;
+
+//read the file and return string with all the contents
+const getFileContent = (path: string) => {
+  return readFileSync(path).toString();
+};
+
+//split entire string into an array of lines
+const splitIntoLines = (path: string) => {
+  const lines: string[] = getFileContent(path).split("\n");
+  return lines;
+};
+
+//match html tags on each line and save them to an array
+//split each line by the tag regex and save the rest into the same array of tokens as the htmlTags
+//returns array of Token objects
+const createTokens: (path: string) => Token[] = (path: string) => {
+  const semiColonRegx: RegExp = /::::/;  
+  const lines = splitIntoLines(path);
+  const emptyStr: string = "";
+  const filteredArr: string[] = lines.filter((line) => line !== emptyStr);
+  const tokens: Token[] = filteredArr.map((line) => {
+    const tag: string = regexTag.exec(line)[0];
+    //removes both the tag and semicolons from the content but could use refactor.
+    const content: string = line.split(regexTag).join('').split(semiColonRegx).join('').trim();
+    const token: Token = {
+      tag: tag,
+      content: content,
+    };
+    return token;
+  });
+
+  return tokens;
+};
+
+console.time('test');
+console.log(createTokens("/Users/brian/Projects/projects/baml/test.baml"));
+console.timeEnd('test');
