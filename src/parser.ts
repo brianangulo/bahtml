fcdcd40e04558f5e141a425afdc8c14fd5789904
Brian Angulo <brianangulo96@gmail.com>
2021-09-04 19:16:12 -0500

    built parser that spits formatted str

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,20 @@
-//parser must return a fully composed and html formatted string
+import { createTokens } from "./lexer.js";
+
+export type HTMLLines = string[];
+
+//create tags
+const createTags = (tag: string) => {
+  return [`<${tag}>`, `</${tag.split(" ")[0]}>`];
+};
 
 //iterate through tokens and create nodes with content inside
+export const createHtmlLines: (path: string) => HTMLLines = (path: string) => {
+  const tokens = createTokens(path);
+  const htmlLines = tokens.map((token, index) => {
+    const [leftTag, rightTag] = createTags(token.tag);
+    const content = token.content;
+    return `${leftTag}${content}${rightTag}`;
+  });
 
-//maybe unify content into a single string?
+  return htmlLines;
+};
